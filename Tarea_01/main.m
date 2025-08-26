37761754715cb10ffa173fab6530445d1f08c1c7
LK PC <krombergerluca@gmail.com>
2025-08-26 09:09:31 -0300

    Agregado de diferencia entre señales y superposicion con señal original

--- Tarea_01/main.m
+++ Tarea_01/main.m
@@ -1,6 +1,6 @@
 %Parametros iniciales
 T = 1;
-N = 100;
+N = 4;
 smpl = 1000*N;
 DC = 50;
 t = linspace(-T,T,smpl).';
@@ -21,13 +21,13 @@ sync(k_sync==0) = DC/100;
 sync(k_sync~=0) = sin(pi*k_sync(k_sync~=0)*DC/100)...
     ./(pi*k_sync(k_sync~=0)).* exp(-1j*pi*k_sync(k_sync~=0)*DC/100);
 %Señal Temporal
-subplot(2,2,1)
+subplot(3,2,1)
 plot(t,x,'linewidth',1.5)
 grid on, ylim([-0.2 1.2])
 xlabel('t'), ylabel('x(t)')
 title('Onda cuadrada unipolar')
 %Modulo Coeficientes de Fourier
-subplot(2,2,2)
+subplot(3,2,2)
 stem(k, abs(C_k),'filled')
 hold on
 plot(k_sync, abs(sync),'r--','LineWidth',1.5,'DisplayName','|sync|')
@@ -36,7 +36,7 @@ hold off
 xlabel('k'), ylabel('|C_k|')
 title('Módulo de los coeficientes de Fourier')
 %Fase Coeficientes de Fourier
-subplot(2,2,4)
+subplot(3,2,4)
 stem(k,angle(C_k),'filled','LineWidth',1.5)
 hold on
 plot(linspace(-N,N,1000*N),angle(sync),'r--')
@@ -44,9 +44,16 @@ grid on
 hold off
 xlabel('k'), ylabel('Fase(C_k) [rad]')
 title('Fase de los Coeficientes de Fourier')
-%Señal Recuperada
-subplot(2,2,3)
-plot(t,x_rec,'-','LineWidth',1.5)
+%Señal Recuperada y Superposicion con Original
+subplot(3,2,3)
+plot(t,x_rec,'-',t,x,'--','LineWidth',1.5)
 grid on
 xlabel('t'), ylabel('x(t)')
 title(sprintf('Reconstrucción con N = %d armónicos', N))
+%Diferencia entre original y reconstruida
+subplot(3,2,5)
+dif = x_rec-x;
+plot(t,dif,'-','LineWidth',1.5)
+grid on
+xlabel('t'), ylabel('Diferencia')
+title('Diferencia entre Original y Reconstruida')
