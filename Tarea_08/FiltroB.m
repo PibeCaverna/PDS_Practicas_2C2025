e2e3a65266ad907c42f1adfc4d5e328f0ab00168
PibeCaverna <francoafalconieri@gmail.com>
2025-10-04 20:01:21 -0300

    Iniciada la Tarea 8

---
+++ Tarea_08/FiltroB.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = FiltroB(inputArg1,inputArg2)
+%FILTROB Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
