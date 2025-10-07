b026c16a2f959100ec5829b12bf021004d9de2bb
Franco <francoafalconieri@gmail.com>
2025-10-07 18:32:00 -0300

    Listo el punto 3 de la tarea 08

--- Labo02/README.md
+++ Labo02/README.md
@@ -1,1 +1,2 @@
 # Laboratorio 02-Sistemas Discretos Simples
+link del latex https://www.overleaf.com/6141697238ffdtqgcknpgm#201f30
--- Tarea_08/Tarea08.m
+++ Tarea_08/Tarea08.m
@@ -7,10 +7,14 @@ CutC = [23*pi/40 33*pi/40]; CutD = [0 9*pi/20];
 freq = {CutA CutB CutC CutD};
 %Muestras por defecto
 N = 300;
+%Archivo de entrada
+global IN;
 
 %Opciones
 opts = {'Respuestas en frecuencia', ...
-        'Bandas'};
+        'Bandas respuesta frecuencia', ...
+        'Temporal filtrado (sin desplazar)', ...
+        'Bandas de frecuencia (sin desplazar)'};
 
 %GUI para ver todo
 Origen = figure(Name='Tarea_08', ...
@@ -35,20 +39,30 @@ Ntxt = uicontrol(Opciones, ...
                  Style='text', ...
                  String='N = ', ...
                  Units='normalized', ...
-                 Position=[.1 .84 .3 .05])
+                 Position=[.1 .84 .3 .05]);
 Ncambia = uicontrol(Opciones, ...
                     Style='edit', ...
                     String=num2str(N), ...
                     Units='normalized', ...
                     Position=[.35 .85 .45 .05]);
+IN_Select = uicontrol(Opciones, ...
+                      Style='popupmenu', ...
+                      String={dir(fullfile('AudioIN','*.mat')).name}, ...
+                      Units='normalized', ...
+                      Position=[.1 .79 .8 .05]);
 %Asignación de Callbacks
 set(Selector,Callback=@(src,~) ... 
     cambiar_plot(src,Ejes,freq,str2double(Ncambia.String)));
 set(Ncambia,Callback=@(src,~) ...
     cambiar_plot(Selector,Ejes,freq,str2double(src.String)));
-
+set(IN_Select,Callback=@(src,~) ...
+    cambiar_entrada(src));
+%Inicia la APP  
+cambiar_plot(Selector,Ejes,freq,str2double(Ncambia.String));
+cambiar_entrada(IN_Select);
 %Callbacks
 function cambiar_plot(src,ax,freq, N)
+  global IN;
   if isnan(N) || N <= 0
     N = 1;
   end
@@ -65,7 +79,19 @@ function cambiar_plot(src,ax,freq, N)
     case 2
       frsimple({FiltA FiltB FiltC FiltD}, ...
                {'Filtro A' 'Filtro B' 'Filtro C' 'Filtro D'}, ax);
+    case 3
+      bandpass({FiltA FiltB FiltC FiltD}, IN.xe, IN.Fs, ...
+               {'Banda A' 'Banda B' 'Banda C' 'Banda D'}, ax);
+    case 4
+      freqband({FiltA FiltB FiltC FiltD}, IN.xe, IN.Fs, ...
+               {'Banda A' 'Banda B' 'Banda C' 'Banda D'}, ax);
   end
+end
+function cambiar_entrada(src)
+  global IN;
+  IN = open(fullfile('AudioIN',src.String{src.Value}));
+  IN.xe = inversor_freq(IN.xe);
+  IN.xe = fliplr(IN.xe);
 end
 %Funciones del programa
 function frsponse(H,Nombre,ax)
@@ -94,3 +120,40 @@ function frsimple(H,Nombre,ax)
   legend(ax,Nombre, 'Location', 'best');
   hold(ax,'off');
 end
+
+function bandpass(filtros,senal,fs,leyenda,ax)
+  M = numel(filtros);
+  t = 0:length(senal)-1;
+  t = t/fs;
+  hold(ax,'on');
+  xlabel(ax,'Tiempo [s]');
+  ylabel(ax,'Amplitud');
+  for k = 1:M
+    S = filter(filtros{k},1,senal);
+    plot(ax,t,S,'LineWidth',1.2)
+  end
+  legend(ax,leyenda,'Location','best');
+  hold(ax,'off');
+end
+function freqband(filtros,senal,fs,leyenda,ax)
+  M = numel(filtros);
+  t = 0:length(senal)-1;
+  t = t/fs;
+  hold(ax,'on');
+  xlabel(ax,'Frecuencia \times \pi [rad/muestra]');
+  ylabel(ax,'Amplitud');
+  for k = 1:M
+    [h,w] = freqz(filter(filtros{k},1,senal));
+    plot(ax,w/pi,abs(h),'LineWidth',1.2)
+  end
+  legend(ax,leyenda,'Location','best');
+  hold(ax,'off');
+end
+
+function invfreq = inversor_freq(senal)
+  N = length(senal);
+  n = 0:N-1;
+
+  invfreq = senal.*((-1).^n)';
+end
+
