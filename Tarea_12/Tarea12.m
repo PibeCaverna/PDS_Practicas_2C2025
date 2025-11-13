eea99a44ad5c703d4dc2a3d7f588d093d3044eb0
Franco <francoafalconieri@gmail.com>
2025-11-13 19:18:33 -0300

    todos los plots del 5.22 implementados, falta el 7

--- Tarea_12/Tarea12.m
+++ Tarea_12/Tarea12.m
@@ -1,23 +1,28 @@
 %%% Tarea 12: Análisis espectral de señales discretas
-clear;
-close all;
-clc;
+clear; close all; clc;
 %% Carga de datos, Defino constantes
 %cantidad de muestras
 N = 100;
 %señal x, carga la variable de una
 load("senal0010_0100.mat");
+%eje de frecuencia
+w = linspace(0,pi,N/2);
+tickw = {[0 pi/4 pi/2 3*pi/4 pi],...
+        {"0", "\pi/4", "\pi/2", "3\pi/4", "\pi"}};
 %% 1_ fft de  la señal
 X = fft(x,N);
 %_________________________________________
 figure('Name','Espectro fft');          %|
-plot(linspace(0,2*pi,length(X)),abs(X));%|
-grid on;                                %|
-xlim([0 pi]);                           %|
+plot(w,abs(X(1:N/2)));                  %|
+grid on;  xlim([0 pi]);                 %|
 xlabel('\omega');                       %|
+xticks(tickw{1});                       %|
+xticklabels(tickw{2});                  %|
 ylabel('|X(e^{j\omega})|');             %|
 %-----------------------------------------
 %% 2_ Módulo del espectro mediante ventanas
+%Numero de la FFT
+NFFT = N;
 %Definimos las ventanas
 ventanas = {};
 ventanas{1} = {rectwin(N),             'Rectangular'};
@@ -38,85 +43,107 @@ end                                                 %|
 %-----------------------------------------------------
 
 %Aproximamos el espectro ventaneado
+x100 = {};
 X100 = {};
 for k = 1:length(ventanas)
-  X100{k} = {fft(x.*ventanas{k}{1}'),ventanas{k}{2}};
+  x100{k} = x.*ventanas{k}{1}'; 
+  X100{k} = fft(x100{k},NFFT);
 end
 %ploteamos las aproximaciones_________________________________________
 figure('Name', 'espectros ventaneados N =100, NFFT = N');           %|
 hold on;                                                            %|
 title('espectros ventaneados N =100, NFFT = N');                    %|
 for k = 1:length(X100)                                              %|
-  plot(linspace(0,2*pi,N),abs(X100{k}{1}),'DisplayName',X100{k}{2});%|
+  plot(w,abs(X100{k}(1:NFFT/2)),'DisplayName',ventanas{k}{2});      %|
 end                                                                 %|
-plot(linspace(0,2*pi,N),abs(X),'DisplayName','fft directo');        %|
-hold off                                                            %|
-grid on;                                                            %|
-xlabel('\omega');                                                   %|
-ylabel('|X(e^{j\omega})|');                                         %|
+hold off; grid on; xticks(tickw{1}); xticklabels(tickw{2});         %|
+xlabel('\omega'); ylabel('|X(e^{j\omega})|');                       %|
 legend('show')                                                      %|
-xlim([0 pi]);                                                       %|
 %---------------------------------------------------------------------
 
 %% 3_ Espectros escalados 
+%Aca escalamos los espectros. Según Nacho y GPT con 2/N alcanza pa
+%Todo, en 5.B del apunte habla de lobulos pero no lo entendí
+X100scl = {};
 for k = 1:length(X100)
-  X100{k}{1} = (X100{k}{1}*2/N);
+  X100scl{k} = abs(X100{k})*2/N;
 end
 %ploteamos las aproximaciones_____________________________________________
 figure('Name', 'espectros ventaneados y escalados N = 100, NFFT = N');  %|
 title('espectros ventaneados y escalados N = 100, NFFT = N');           %|
-for k = 1:length(X100)                                                  %|
+for k = 1:length(X100scl)                                               %|
   subplot(2,2,k);                                                       %|
-  hold on;                                                              %|
-  semilogy(linspace(0,2*pi,N),abs(X100{k}{1}),'DisplayName',X100{k}{2});%|
-  title(ventanas{k}{2});                                                %|
-  hold off                                                              %|
-  grid on;                                                              %|
-  xlabel('\omega');                                                     %|
-  ylabel('|X(e^{j\omega})|');                                           %|
-  legend('show')                                                        %|
-  xlim([0 pi]);                                                         %|
+  semilogy(w,X100scl{k}(1:N/2)); title(ventanas{k}{2});                 %|
+  xlabel('\omega');  ylabel('|X(e^{j\omega})|');                        %|
+  xticks(tickw{1}); xticklabels(tickw{2});                              %|
 end                                                                     %|
 %-------------------------------------------------------------------------
 
-%% 4_ Inciso 2 con N = 1000
+%% 4_ Inciso 2 con NFFT = 1000
 NFFT = 1000;
 %Aproximamos el espectro ventaneado
+w1k = linspace(0,pi,NFFT/2);
+x1000 = {};
 X1000 = {};
 for k = 1:length(ventanas)
-  X1000{k} = {fft(x.*ventanas{k}{1}',NFFT),ventanas{k}{2}};
+  x1000{k} = x.*ventanas{k}{1}'; 
+  X1000{k} = fft(x100{k},NFFT);
+end
+%ploteamos las aproximaciones_________________________________________
+figure('Name', 'espectros ventaneados N =100, NFFT = 1000');        %|
+hold on;                                                            %|
+title('espectros ventaneados N =100, NFFT = 1000');                 %|
+for k = 1:length(X1000)                                             %|
+  plot(w1k,abs(X1000{k}(1:NFFT/2)),'DisplayName',ventanas{k}{2});   %|
+end                                                                 %|
+hold off; grid on; xticks(tickw{1}); xticklabels(tickw{2});         %|
+xlabel('\omega'); ylabel('|X(e^{j\omega})|');                       %|
+legend('show')                                                      %|
+%---------------------------------------------------------------------
+%Escalamos
+X1000scl = {};
+for k = 1:length(X1000)
+  X1000scl{k} = abs(X1000{k})*2/N;
 end
 %ploteamos las aproximaciones______________________________________________
-figure('Name', 'espectros ventaneados N = 100, NFFT = 1000');            %|
-title('espectros ventaneados N = 100, NFFT = 1000');                     %|
-hold on;                                                                 %|
-for k = 1:length(X1000)                                                  %|
-  plot(linspace(0,2,NFFT),abs(X1000{k}{1}),'DisplayName',X1000{k}{2});   %|
+figure('Name', 'espectros ventaneados y escalados N = 100, NFFT = 1000');%|
+title('espectros ventaneados y escalados N = 100, NFFT = 1000');         %|
+for k = 1:length(X1000scl)                                               %|
+  subplot(2,2,k);                                                        %|
+  semilogy(w1k,X1000scl{k}(1:NFFT/2)); title(ventanas{k}{2});            %|
+  xlabel('\omega');  ylabel('|X(e^{j\omega})|');                         %|
+  xticks(tickw{1}); xticklabels(tickw{2});                               %|
 end                                                                      %|
-hold off                                                                 %|
-grid on;                                                                 %|
-xlabel('\omega');                                                        %|
-ylabel('|X(e^{j\omega})|');                                              %|
-legend('show')                                                           %|
-xlim([0 1]);                                                             %|
 %--------------------------------------------------------------------------
 
-for k = 1:length(X1000)
-  X1000{k}{1} = (X1000{k}{1}*2/NFFT);
-end
-%ploteamos las aproximaciones_______________________________________________
-figure('Name', 'espectros ventaneados y escalados N = 100, NFFT = 1000'); %|
-title('espectros ventaneados y escalados N = 100, NFFT = 1000')           %|
-for k = 1:length(X1000)                                                   %|
-  subplot(2,2,k);                                                         %|
-  hold on;                                                                %|
-  semilogy(linspace(0,2,NFFT),abs(X1000{k}{1}),'DisplayName',X1000{k}{2});%|
-  title(ventanas{k}{2});                                                  %|
-  hold off                                                                %|
-  grid on;                                                                %|
-  xlabel('\omega');                                                       %|
-  ylabel('|X(e^{j\omega})|');                                             %|
-  legend('show')                                                          %|
-  xlim([0 1]);                                                            %|
-end                                                                       %|
-%---------------------------------------------------------------------------
+%% 5_ Tabla con frecuencias normalizadas
+% Elijo todos los picos locales, pero hay algunos que son muy tenues
+% Los agarre a mano, puede que esten un toq mal
+rctabla = {'Rectangular',...
+           [0.503662 2.04429  1.08036 0.132646],...
+           [0.202693 0.963254 1.29693 2.26018]};
+hatabla = {'Hamming',...
+           [0.0606319 1.08201  0.544943 0.0470894],...
+           [0.509958  0.963254 1.29693  2.25389]};
+vhtabla = {'Von Hann',...
+           [0.0496613 0.998329 0.498469 0.0399608],...
+           [0.522549  0.963254 1.29693  2.24759],};
+bltabla = {'Blackman',...
+           [0.0419 0.8391 0.4195 0.0043 0.0336],...
+           [0.5163 0.9633 1.2969 1.7125 2.2476]};
+% Construcción de tablas individuales
+T_rect = table(rctabla{2}(:), rctabla{3}(:)/pi, ...
+    'VariableNames', {'Amplitud', 'f_norm_(×π)'});
+T_ham = table(hatabla{2}(:), hatabla{3}(:)/pi, ...
+    'VariableNames', {'Amplitud', 'f_norm_(×π)'});
+T_hann = table(vhtabla{2}(:), vhtabla{3}(:)/pi, ...
+    'VariableNames', {'Amplitud', 'f_norm_(×π)'});
+T_blk = table(bltabla{2}(:), bltabla{3}(:)/pi, ...
+    'VariableNames', {'Amplitud', 'f_norm_(×π)'});
+% Mostrar tablas en consola
+disp('==== Ventana Rectangular ===='), disp(T_rect)
+disp('==== Ventana Hamming ===='),    disp(T_ham)
+disp('==== Ventana Von Hann ===='),   disp(T_hann)
+disp('==== Ventana Blackman ===='),   disp(T_blk)
+
+%% 7_ Reconstruccion y sintesis mediante ventana de hamming
